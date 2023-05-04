68168192015bea113990eb79fd4d584891281fff
Slava Monich <slava@monich.com>
2023-05-05 02:44:52 +0300

    [foilauth] Extracted common part of TOTP and HOTP into a separate call
    And eliminated dependency on QQmlEngine

--- src/FoilAuth.cpp
+++ src/FoilAuth.cpp
@@ -1,34 +1,36 @@
 /*
- * Copyright (C) 2019-2022 Jolla Ltd.
  * Copyright (C) 2019-2023 Slava Monich <slava@monich.com>
+ * Copyright (C) 2019-2022 Jolla Ltd.
  *
- * You may use this file under the terms of BSD license as follows:
+ * You may use this file under the terms of the BSD license as follows:
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *   1. Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer
- *      in the documentation and/or other materials provided with the
- *      distribution.
- *   3. Neither the names of the copyright holders nor the names of its
- *      contributors may be used to endorse or promote products derived
- *      from this software without specific prior written permission.
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer
+ *     in the documentation and/or other materials provided with the
+ *     distribution.
+ *  3. Neither the names of the copyright holders nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING
+ * IN ANY WAY OUT OF THE USE OR INABILITY TO USE THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * any official policies, either expressed or implied.
  */
 
 #include "FoilAuth.h"
@@ -46,10 +48,8 @@
 
 #include "gutil_misc.h"
 
-#include <QStringList>
 #include <QFile>
 #include <QFileSystemWatcher>
-#include <QQmlEngine>
 
 #define FOILAPPS_DIR    "/usr/bin"
 #define FOILPICS_PATH   FOILAPPS_DIR "/harbour-foilpics"
@@ -274,7 +274,7 @@ FoilAuth::TOTP(
     uint aMaxPass,
     DigestAlgorithm aAlgorithm)
 {
-    return FoilAuth::HOTP(aSecret, aTime/PERIOD, aMaxPass, aAlgorithm);
+    return FoilAuth::hash(aSecret, aTime/PERIOD, aAlgorithm) % aMaxPass;
 }
 
 uint
@@ -284,7 +284,16 @@ FoilAuth::HOTP(
     uint aMaxPass,
     DigestAlgorithm aAlgorithm)
 {
-    const guint64 msg = htobe64(aCounter);
+    return FoilAuth::hash(aSecret, aCounter, aAlgorithm) % aMaxPass;
+}
+
+uint
+FoilAuth::hash(
+    const QByteArray aSecret,
+    quint64 aValue,
+    DigestAlgorithm aAlgorithm)
+{
+    const guint64 msg = htobe64(aValue);
     GType (*digest_type)(void) = foil_impl_digest_sha1_get_type;
     switch (aAlgorithm) {
     case DigestAlgorithmSHA1:
@@ -307,7 +316,7 @@ FoilAuth::HOTP(
     const uint mini_hash = be32toh(*(guint32*)(hash + offset)) & 0x7fffffff;
 
     g_bytes_unref(hash_bytes);
-    return mini_hash % aMaxPass;
+    return mini_hash;
 }
 
 QString
